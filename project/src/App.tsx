8c0c037bac680f286b19b96f62dac274e7ca9585
khaleed-123 <atobajayeajani6.com>
2025-02-04 19:43:14 +0100

    Cart and App is modify

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -7,6 +7,7 @@ import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Menu from './pages/Menu';
 import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
 import { AuthProvider } from './context/AuthContext';
 import { CartProvider } from './context/CartContext';
 
@@ -24,6 +25,7 @@ function App() {
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/menu" element={<Menu />} />
                 <Route path="/cart" element={<Cart />} />
+                <Route path="/checkout" element={<Checkout />} />
               </Routes>
             </main>
             <Footer />
